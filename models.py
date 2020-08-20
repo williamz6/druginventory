55a3c326db5f4ca914b7b83d070fd67850eb0990
Otuekong William <52850689+williamz6@users.noreply.github.com>
2020-08-20 19:23:43 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,39 @@
+
+class Drug(db.Model):
+    __tablename__= "drugs"
+    id = db.Column(db.Integer, primary_key=True)
+    drug_name = db.Column(db.String(100), unique=True)
+    quantity= db.Column(db.Integer, nullable=False)
+
+    def __init__(self, drug_name, quantity):
+
+        self.drug_name = drug_name
+        self.quantity = quantity
+
+class Transaction(db.Model):
+    __tablename__= "transactions"
+    id = db.Column(db.Integer, primary_key=True)
+    drug_id = db.Column(db.Integer(), db.ForeignKey('drugs.id'))
+    quantity= db.Column(db.Integer, nullable=False)
+    transaction_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    drugs = relationship("Drug", secondary="movements")
+
+class Movement(db.Model):
+  __tablename__ = 'movements'
+  id = db.Column(db.Integer(), primary_key=True)
+  drug_id = db.Column(db.Integer, db.ForeignKey('drugs.id'), primary_key=True)
+  t_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), primary_key=True)
+  quantity = db.Column(db.Integer)
+
+  drug = relationship("Drug", backref=backref("movements", cascade="all, delete-orphan" ))
+  transaction = relationship("Transaction", backref=backref("movements", cascade="all, delete-orphan" ))
+
+  def __init__(self, drug=None, quantity=None):
+    
+    self.drug = drug
+   
+    self.quantity = quantity
+
+  def __repr__(self):
+    return '<Movement {}>'.format(self.drug.name)
